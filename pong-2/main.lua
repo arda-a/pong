fe23b8231272acc0bb698f5e263bf1ce710ecd05
arda-a <6350204+arda-a@users.noreply.github.com>
2019-03-06 22:26:17 +1100

    refactoring & version update

--- pong-2/main.lua
+++ pong-2/main.lua
@@ -19,13 +19,12 @@
     resolution, though in widescreen (16:9) so it looks nicer on 
     modern systems.
 ]]
-
 -- push is a library that will allow us to draw our game at a virtual
 -- resolution, instead of however large our window is; used to provide
 -- a more retro aesthetic
 --
 -- https://github.com/Ulydev/push
-push = require 'push'
+push = require "push"
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -37,20 +36,26 @@ VIRTUAL_HEIGHT = 243
     Runs when the game first starts up, only once; used to initialize the game.
 ]]
 function love.load()
-    love.graphics.setDefaultFilter('nearest', 'nearest')
+    love.graphics.setDefaultFilter("nearest", "nearest")
 
     -- more "retro-looking" font object we can use for any text
-    smallFont = love.graphics.newFont('font.ttf', 8)
+    smallFont = love.graphics.newFont("font.ttf", 8)
 
     -- set LÖVE2D's active font to the smallFont obect
     love.graphics.setFont(smallFont)
 
     -- initialize window with virtual resolution
-    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
-        fullscreen = false,
-        resizable = false,
-        vsync = true
-    })
+    push:setupScreen(
+        VIRTUAL_WIDTH,
+        VIRTUAL_HEIGHT,
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+        {
+            fullscreen = false,
+            resizable = false,
+            vsync = true
+        }
+    )
 end
 
 --[[
@@ -59,7 +64,7 @@ end
 ]]
 function love.keypressed(key)
     -- keys can be accessed by string name
-    if key == 'escape' then
+    if key == "escape" then
         -- function LÖVE gives us to terminate application
         love.event.quit()
     end
@@ -71,14 +76,14 @@ end
 ]]
 function love.draw()
     -- begin rendering at virtual resolution
-    push:apply('start')
+    push:apply("start")
 
     -- clear the screen with a specific color; in this case, a color similar
     -- to some versions of the original Pong
-    love.graphics.clear(40, 45, 52, 255)
+    love.graphics.clear(40/255, 45/255, 52/255, 1)
 
     -- draw welcome text toward the top of the screen
-    love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf("Hello Pong!", 0, 20, VIRTUAL_WIDTH, "center")
 
     --
     -- paddles are simply rectangles we draw on the screen at certain points,
@@ -86,14 +91,14 @@ function love.draw()
     --
 
     -- render first paddle (left side)
-    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    love.graphics.rectangle("fill", 10, 30, 5, 20)
 
     -- render second paddle (right side)
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+    love.graphics.rectangle("fill", VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
 
     -- render ball (center)
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+    love.graphics.rectangle("fill", VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
     -- end rendering at virtual resolution
-    push:apply('end')
+    push:apply("end")
 end
